a9c26cee48a371b9e4bf98439257e1b3c67e58be
gery <gerardromero@ceroca.cat>
2024-09-23 19:55:19 +0200

    moviment jugador

---
+++ Assets/scrips/naujugador.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NauJugador : MonoBehaviour
+{
+    private float _vel;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _vel = 15;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float direccioIndicadaX = Input.GetAxisRaw("Horizontal");
+        float direccioIndicadaY = Input.GetAxisRaw("Vertical");
+        Debug.Log("X: " + direccioIndicadaX + "Y: " + direccioIndicadaY);
+        Vector2 direccioIndicada = new Vector2(direccioIndicadaX, direccioIndicadaY);
+
+        Vector2 novaPos = transform.position;//transform.position: pos actual de la nau
+        novaPos = novaPos + direccioIndicada * _vel * Time.deltaTime;
+        transform.position = novaPos;
+    }
+}
