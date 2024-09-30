2c8e7da804087d18b3e691b1d46b160ce6e373c4
gery24 <150917721+gery24@users.noreply.github.com>
2024-09-30 20:19:43 +0200

    nau disparant

--- Assets/scrips/naujugador.cs
+++ Assets/scrips/naujugador.cs
@@ -7,6 +7,8 @@ public class NauJugador : MonoBehaviour
     private float _vel;
 
     Vector2 minPantalla, maxPantalla;
+
+    [SerializeField] private GameObject prefabProjectil;
     
     // Start is called before the first frame update
     void Start()
@@ -14,10 +16,36 @@ public class NauJugador : MonoBehaviour
         _vel = 15;
         minPantalla = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
         maxPantalla = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+
+        float meitatMidaImatgeX = GetComponent<SpriteRenderer>().sprite.bounds.size.x * transform.localScale.x / 2;
+        float meitatMidaImatgeY = GetComponent<SpriteRenderer>().sprite.bounds.size.y * transform.localScale.y / 2;
+
+        minPantalla.x = minPantalla.x + meitatMidaImatgeX;
+        maxPantalla.x = maxPantalla.x - meitatMidaImatgeX;
+
+        //Esto es lo mismo de arriba puesto de otra manera
+
+        minPantalla.y += meitatMidaImatgeY;
+        maxPantalla.y -= meitatMidaImatgeY;
     }
 
     // Update is called once per frame
     void Update()
+    {
+        MoureNau();
+        DisparaProjectil();
+    }
+
+    private void DisparaProjectil()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            GameObject projectil = Instantiate(prefabProjectil);
+            projectil.transform.position = transform.position;
+        }
+    }
+
+    private void MoureNau()
     {
         float direccioIndicadaX = Input.GetAxisRaw("Horizontal");
         float direccioIndicadaY = Input.GetAxisRaw("Vertical");
