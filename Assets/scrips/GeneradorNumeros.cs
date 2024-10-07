f35cf4c0e740d92dbd55fd78079454cf56c79e21
gery24 <150917721+gery24@users.noreply.github.com>
2024-10-07 17:56:27 +0200

    genera nums aleatoris

--- Assets/scrips/GeneradorNumeros.cs
+++ Assets/scrips/GeneradorNumeros.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 public class GeneradorNumeros : MonoBehaviour
 {
     [SerializeField] private GameObject prefabNum;
+    private Vector2 minpantalla, maxpantalla;
+
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("GenerarNumero", 1f, 2f);
+        minpantalla = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+        maxpantalla = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
     }
 
     private void GenerarNumero()
     {
+        GameObject numero = Instantiate(prefabNum);
+
+        numero.transform.position = new Vector2(Random.Range(minpantalla.x, maxpantalla.x), maxpantalla.y);
 
     }
 
--- Assets/scrips/Numero.cs
+++ Assets/scrips/Numero.cs
@@ -7,11 +7,20 @@ public class Numero : MonoBehaviour
     private float vel;
 
     private Vector2 minPantalla;
+
+    [SerializeField] private Sprite[] arraySpritesNumeros = new Sprite[10];
+
+    private int valorNumero;
+
     // Start is called before the first frame update
     void Start()
     {
         vel = 3f;
         minPantalla = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+
+        System.Random numAleatori = new System.Random();
+        valorNumero = numAleatori.Next(0, 10);
+        GetComponent<SpriteRenderer>().sprite = arraySpritesNumeros[valorNumero];
     }
 
     // Update is called once per frame
